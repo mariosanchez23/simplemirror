c4c49b9d4d18af2aaee7e3431b6c62d067a9f2c8
IRISMeister <12710874+IRISMeister@users.noreply.github.com>
2021-05-25 16:38:58 +0900

    ability to join as DR member

--- start.sh
+++ start.sh
@@ -5,6 +5,8 @@ docker-compose exec -T ap1a bash -c "\$ISC_PACKAGE_INSTALLDIR/dev/Cloud/ICM/wait
 docker-compose exec -T ap2a bash -c "\$ISC_PACKAGE_INSTALLDIR/dev/Cloud/ICM/waitISC.sh '' 60"
 # join backups
 docker-compose up -d ap1b ap2b
+docker-compose exec -T ap1b bash -c "\$ISC_PACKAGE_INSTALLDIR/dev/Cloud/ICM/waitISC.sh '' 60"
+docker-compose exec -T ap2b bash -c "\$ISC_PACKAGE_INSTALLDIR/dev/Cloud/ICM/waitISC.sh '' 60"
 # webgws and LBs
 docker-compose up -d webgw1 webgw2 nginx haproxy
 # give them a moment
